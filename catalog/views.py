1f7df7fa44520bccf24ae7e48f6971b4d3f043e1
Sadie <an5dy90@gmail.com>
2024-10-10 09:06:47 +0000

    author_list does not work

--- catalog/views.py
+++ catalog/views.py
@@ -30,6 +30,15 @@ def index(request):
 
 class BookListView(generic.ListView):
     model = Book
+    paginate_by = 2
 
 class BookDetailView(generic.DetailView):
     model = Book
+
+class AuthorListView(generic.ListView):
+    model = Author
+    paginate_by = 2
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
+    
